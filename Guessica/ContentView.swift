c415303099e6023679b043cba15eb86fe9138323
Maxim Vainikka <rithmique@yahoo.com>
2021-04-20 20:46:27 +1000

    Initial Commit

---
+++ Guessica/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guessica
+//
+//  Created by Максим Нуждин on 20.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guessica/GuessicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessicaApp.swift
+//  Guessica
+//
+//  Created by Максим Нуждин on 20.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
