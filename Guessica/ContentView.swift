e85c651372c924a87fec8e6d220d3d59cb5d542c
Maxim Vainikka <rithmique@yahoo.com>
2021-04-27 22:10:44 +0200

    updated with rotation(copypaste, im useless)

--- Guessica/ContentView.swift
+++ Guessica/ContentView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var correctAnswer = Int.random(in: 0..<3)
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var totalScore = 0
-    @State private var rotationAmount = 360
+    
+    @State private var animateCorrect = 0.0
+    @State private var animateOpacity = 1.0
+    @State private var wrongFlags = false
+    @State private var correctFlag = false
+    @State private var selectedFlag = 0
     
     var body: some View {
         ZStack {
@@ -33,18 +38,22 @@ struct ContentView: View {
                 .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 
-                ForEach(0..<3) { number in
+                ForEach(0..<3, id: \.self) { number in
                     Button(action: {
                         withAnimation {
-                        flagPressed(number)
-                        showingScore.toggle()
+                            selectedFlag = number
+                            flagPressed(number)
+                            showingScore.toggle()
                         }
                     }, label: {
                         countryImageLabel(image: countries[number])
                             .rotation3DEffect(
-                                .degrees(Double(rotationAmount)),
-                                axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/)
-                            
+                                .degrees(number == correctAnswer ? animateCorrect : 0), axis: (x: 0, y: 1, z: 0))
+                            .opacity(number != correctAnswer && correctFlag ? animateOpacity : 1)
+                            .background(wrongFlags && selectedFlag == number ? Capsule(style: .circular).fill(Color.red).blur(radius: 30) : Capsule(style: .circular).fill(Color.clear).blur(radius: 0))
+                            .opacity(wrongFlags && selectedFlag != number ? animateOpacity : 1)
+                        
+                        
                     })
                     .padding()
                 }
@@ -63,15 +72,25 @@ struct ContentView: View {
         if number == correctAnswer {
             totalScore += 1
             scoreTitle = "Correct"
-            rotationAmount += 360
+            withAnimation {
+                animateCorrect += 360
+                animateOpacity = 0.25
+                correctFlag = true
+            }
         } else {
             scoreTitle = "Wrong! That is \(countries[number])"
+            withAnimation {
+                animateOpacity = 0.25
+                wrongFlags = true
+            }
         }
     }
     
     func askQuestion() {
+        wrongFlags = false
+        correctFlag = false
         countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        correctAnswer = Int.random(in: 0..<3)
     }
 }
 
